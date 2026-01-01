782c4d9781f5cbf2cf51e7f71129c9a806682485
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2026-01-01 23:17:43 +0100

    binaryfileconvert.py decoded.png : python script to convert binaryfile.raw to decoded PNG file

---
+++ binaryfileconvert.py
@@ -0,0 +1,22 @@
+from PIL import Image
+import math
+
+data = open("binaryfile.raw","rb").read()
+width = 285
+height = 24
+pages = math.ceil(height/8)
+assert len(data) == width * pages
+
+img = Image.new("1", (width, height))
+idx = 0
+for page in range(pages):
+    for x in range(width):
+        b = data[idx]; idx += 1
+        for bit in range(8):          # LSB = top pixel in page
+            y = page*8 + bit
+            if y < height:
+                img.putpixel((x,y), (b >> bit) & 1)
+
+img = img.resize((width*4, height*10), Image.NEAREST)
+img.save("decoded.png")
+
